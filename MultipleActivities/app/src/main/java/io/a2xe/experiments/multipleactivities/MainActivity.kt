d340964159367319fa256da3d6bd88c55d5882cc
giorgionatili <g.natili@gnstudio.com>
2018-01-23 06:56:52 -0800

    Fix the UI to input the URL to open

--- MultipleActivities/app/src/main/java/io/a2xe/experiments/multipleactivities/MainActivity.kt
+++ MultipleActivities/app/src/main/java/io/a2xe/experiments/multipleactivities/MainActivity.kt
@@ -2,11 +2,19 @@ package io.a2xe.experiments.multipleactivities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        open_new_window.setOnClickListener {
+
+            launchActivity<WebContentActivity>{
+                putExtra("LAUNCH_URL", url_to_open.text.toString())
+            }
+        }
     }
 }
