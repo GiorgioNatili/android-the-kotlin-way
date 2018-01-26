f3249385677a4640a5039f6e65f26da73a47d8b8
giorgionatili <g.natili@gnstudio.com>
2018-01-25 20:29:33 -0800

    Fix the adapter required for the list of visited websites

---
+++ MultipleActivities/app/src/main/java/io/a2xe/experiments/multipleactivities/adapters/WebSiteAdapter.kt
@@ -0,0 +1,44 @@
+package io.a2xe.experiments.multipleactivities.adapters
+
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import io.a2xe.experiments.multipleactivities.R
+import io.a2xe.experiments.multipleactivities.adapters.renderers.WebSiteItemRenderer
+import io.a2xe.experiments.multipleactivities.model.WebSite
+
+/**
+ * Created by giorgio on 1/25/18.
+ */
+class WebSiteAdapter(private val activity: Activity,
+                     private val history: ArrayList<WebSite>) : BaseAdapter() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        val view: View?
+        val viewHolder: WebSiteItemRenderer
+
+        if (convertView == null) {
+            view = activity.layoutInflater.inflate(R.layout.web_site, parent, false)
+            viewHolder = WebSiteItemRenderer(view)
+            view?.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as WebSiteItemRenderer
+        }
+
+        viewHolder.webSiteTitle.text = history[position].title
+        return view
+    }
+    override fun getItem(position: Int): Any {
+        return history[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return history.size
+    }
+}
---
+++ MultipleActivities/app/src/main/java/io/a2xe/experiments/multipleactivities/adapters/renderers/WebSiteItemRenderer.kt
@@ -0,0 +1,12 @@
+package io.a2xe.experiments.multipleactivities.adapters.renderers
+
+import android.view.View
+import android.widget.TextView
+import io.a2xe.experiments.multipleactivities.R
+
+/**
+ * Created by giorgio on 1/25/18.
+ */
+internal class WebSiteItemRenderer(view: View?) {
+    val webSiteTitle: TextView = view?.findViewById(R.id.web_site_title) as TextView
+}
