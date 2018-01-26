466792e2310d50794d0d34a8d8fb6efd84a6c411
giorgionatili <g.natili@gnstudio.com>
2018-01-25 20:30:14 -0800

    Fix the integration of the custom adapter

--- MultipleActivities/app/src/main/java/io/a2xe/experiments/multipleactivities/MainActivity.kt
+++ MultipleActivities/app/src/main/java/io/a2xe/experiments/multipleactivities/MainActivity.kt
@@ -1,8 +1,11 @@
 package io.a2xe.experiments.multipleactivities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.a2xe.experiments.multipleactivities.adapters.WebSiteAdapter
 import io.a2xe.experiments.multipleactivities.bundles.launchURL
+import io.a2xe.experiments.multipleactivities.utilities.launchActivity
+import io.a2xe.experiments.multipleactivities.utilities.readURLs
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = WebSiteAdapter(this, readURLs())
+        local_history.adapter = adapter
 
         open_new_window.setOnClickListener {
 
