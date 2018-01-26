47e3b45ebd8a8c464b28de2b34636423ccd28ab7
giorgionatili <g.natili@gnstudio.com>
2018-01-25 20:27:23 -0800

    Fix the extension functions needed to save persistent data

---
+++ MultipleActivities/app/src/main/java/io/a2xe/experiments/multipleactivities/utilities/persitingHistory.kt
@@ -0,0 +1,36 @@
+package io.a2xe.experiments.multipleactivities.utilities
+
+import android.content.Context
+import android.preference.PreferenceManager
+import io.a2xe.experiments.multipleactivities.model.WebSite
+
+/**
+ * Created by giorgio on 1/25/18.
+ */
+fun Context.saveURLs (collection: ArrayList<WebSite>) {
+
+    val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+    val editor = sharedPreferences.edit()
+
+    for (i in 0 until collection.size) {
+        editor.remove(collection[i].title)
+        editor.putString(collection[i].title, collection[i].url)
+    }
+
+    editor.apply()
+}
+
+fun Context.readURLs(): ArrayList<WebSite> {
+
+    val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+    val allEntries = sharedPreferences.all
+
+    val urls = arrayListOf<WebSite>()
+
+    for (entry in allEntries.entries) {
+
+        urls.add(WebSite(entry.key, entry.value.toString()))
+    }
+
+    return urls
+}
