12f841e94e7d9d399c59dcaa3d6b403c169d6e59
giorgionatili <g.natili@gnstudio.com>
2018-02-14 07:17:57 -0800

    Clean up the constructor by adding a property to the class

--- MultipleActivities/app/src/main/java/io/a2xe/experiments/multipleactivities/model/WebSite.kt
+++ MultipleActivities/app/src/main/java/io/a2xe/experiments/multipleactivities/model/WebSite.kt
@@ -4,5 +4,6 @@ package io.a2xe.experiments.multipleactivities.model
  * Created by giorgio on 1/25/18.
  */
 data class WebSite(val title: String,
-                   val url: String?,
-                   val timestamp: Long = System.currentTimeMillis())
+                   val url: String) {
+    val timestamp: Long = System.currentTimeMillis()
+}
