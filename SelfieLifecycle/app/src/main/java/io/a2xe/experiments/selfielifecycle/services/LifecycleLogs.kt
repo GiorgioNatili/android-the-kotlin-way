b0e22670d692c030839a7eabae10f6f85ca3310d
giorgionatili <g.natili@gnstudio.com>
2018-02-26 17:10:27 -0800

    Fix the lifecyle observer and the manager that inject it in the app

---
+++ SelfieLifecycle/app/src/main/java/io/a2xe/experiments/selfielifecycle/services/LifecycleLogs.kt
@@ -0,0 +1,27 @@
+package io.a2xe.experiments.selfielifecycle.services
+
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.OnLifecycleEvent
+import io.a2xe.experiments.selfielifecycle.model.LifecycleEventLog
+
+/**
+ * Created by giorgio on 2/25/18.
+ */
+class LifecycleLogs : LifecycleObserver {
+
+    val history = arrayListOf<LifecycleEventLog>()
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    fun onEvent(owner: LifecycleOwner, event: Lifecycle.Event) {
+
+        println("onEvent: ownerState: ${owner.lifecycle.currentState}")
+        println("onEvent: ownerClassName: ${owner.javaClass.canonicalName}")
+        println("onEvent: event: ${event.name}")
+
+        val log = LifecycleEventLog(event.name, owner.javaClass.canonicalName,
+                owner.lifecycle.currentState)
+        history.add(log)
+    }
+}
---
+++ SelfieLifecycle/app/src/main/java/io/a2xe/experiments/selfielifecycle/services/LifecycleObserverManager.kt
@@ -0,0 +1,55 @@
+package io.a2xe.experiments.selfielifecycle.services
+
+import android.app.Activity
+import android.app.Application
+import android.arch.lifecycle.LifecycleObserver
+import android.os.Bundle
+import io.a2xe.experiments.selfielifecycle.utilities.noop
+import io.a2xe.experiments.selfielifecycle.utilities.whenAppCompactActivity
+
+/**
+ * Created by giorgio on 2/26/18.
+ */
+class LifecycleObserverManager(private val lifecycleObserver: LifecycleObserver):
+        Application.ActivityLifecycleCallbacks {
+
+    override fun onActivityPaused(activity: Activity?) {
+        activity?.noop()
+    }
+
+    override fun onActivityResumed(activity: Activity?) {
+        activity?.noop()
+    }
+
+    override fun onActivityStarted(activity: Activity?) {
+        activity?.noop()
+    }
+
+    override fun onActivityDestroyed(activity: Activity?) {
+        activity?.let {
+            it.whenAppCompactActivity(activity) {
+                it.lifecycle.removeObserver(lifecycleObserver)
+            }
+        }
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
+        activity?.noop()
+    }
+
+    override fun onActivityStopped(activity: Activity?) {
+        activity?.let {
+            it.whenAppCompactActivity(activity) {
+                it.lifecycle.removeObserver(lifecycleObserver)
+            }
+        }
+    }
+
+    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+        activity?.let {
+            it.whenAppCompactActivity(activity) {
+                it.lifecycle.addObserver(lifecycleObserver)
+            }
+        }
+    }
+}
