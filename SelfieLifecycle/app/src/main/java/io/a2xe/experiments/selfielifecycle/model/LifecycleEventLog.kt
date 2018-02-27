f648b4f575c8807bcacbbfec62675d589659a405
giorgionatili <g.natili@gnstudio.com>
2018-02-26 17:08:51 -0800

    Fix the value object used to represents the app lifecycles

---
+++ SelfieLifecycle/app/src/main/java/io/a2xe/experiments/selfielifecycle/model/LifecycleEventLog.kt
@@ -0,0 +1,6 @@
+package io.a2xe.experiments.selfielifecycle.model
+
+/**
+ * Created by giorgio on 2/25/18.
+ */
+data class LifecycleEventLog(val name: String, val component: String, val state: Any)
