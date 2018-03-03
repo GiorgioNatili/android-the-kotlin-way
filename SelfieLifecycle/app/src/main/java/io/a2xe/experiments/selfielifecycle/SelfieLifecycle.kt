45e5fe6296af79150577b2617d1a2d8c74c62a9a
giorgionatili <g.natili@gnstudio.com>
2018-03-03 07:09:34 -0800

    Fix the usae of the LifecycleObserverManager passing the callback

--- SelfieLifecycle/app/src/main/java/io/a2xe/experiments/selfielifecycle/SelfieLifecycle.kt
+++ SelfieLifecycle/app/src/main/java/io/a2xe/experiments/selfielifecycle/SelfieLifecycle.kt
@@ -1,8 +1,9 @@
 package io.a2xe.experiments.selfielifecycle
 
 import android.app.Application
-import io.a2xe.experiments.selfielifecycle.services.LifecycleObserverManager
 import io.a2xe.experiments.selfielifecycle.services.LifecycleLogs
+import io.a2xe.experiments.selfielifecycle.services.LifecycleObserverManager
+import io.a2xe.experiments.selfielifecycle.utilities.toast
 
 /**
  * Created by giorgio on 2/25/18.
@@ -13,6 +14,7 @@ class SelfieLifecycle : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        registerActivityLifecycleCallbacks(LifecycleObserverManager(LifecycleLogs()))
+        registerActivityLifecycleCallbacks(LifecycleObserverManager(LifecycleLogs(),
+                {observer -> (observer as LifecycleLogs).all.toast(this) }))
     }
 }
