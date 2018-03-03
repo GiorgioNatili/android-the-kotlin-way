78472def89f12cea895f0f60c423f9a90b4503aa
giorgionatili <g.natili@gnstudio.com>
2018-03-03 07:08:23 -0800

    Fix the logs to expose a string representation of them

--- SelfieLifecycle/app/src/main/java/io/a2xe/experiments/selfielifecycle/services/LifecycleLogs.kt
+++ SelfieLifecycle/app/src/main/java/io/a2xe/experiments/selfielifecycle/services/LifecycleLogs.kt
@@ -11,17 +11,20 @@ import io.a2xe.experiments.selfielifecycle.model.LifecycleEventLog
  */
 class LifecycleLogs : LifecycleObserver {
 
-    val history = arrayListOf<LifecycleEventLog>()
+    private val history = arrayListOf<LifecycleEventLog>()
 
     @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
     fun onEvent(owner: LifecycleOwner, event: Lifecycle.Event) {
 
-        println("onEvent: ownerState: ${owner.lifecycle.currentState}")
-        println("onEvent: ownerClassName: ${owner.javaClass.canonicalName}")
-        println("onEvent: event: ${event.name}")
+        println("onEvent -> ownerState: ${owner.lifecycle.currentState}")
+        println("onEvent -> ownerClassName: ${owner.javaClass.canonicalName}")
+        println("onEvent -> event: ${event.name}")
 
         val log = LifecycleEventLog(event.name, owner.javaClass.canonicalName,
                 owner.lifecycle.currentState)
         history.add(log)
     }
+
+    var all: String = ""
+        get() = history.joinToString("/n/n") { "onEvent -> ownerState: ${it.state}/n onEvent -> ownerClassName: ${it.javaClass}/n onEvent -> event: ${it.name}"}
 }
