ea534f8259cc9418e4949ab039d9108ac3d2258e
giorgionatili <g.natili@gnstudio.com>
2018-04-08 09:48:02 -0700

    Fix the data type of the cards to minimize code bolierplate

--- PokemonDeck/app/src/main/java/io/a2xe/experiments/pokemondeck/model/vos/Deck.kt
+++ PokemonDeck/app/src/main/java/io/a2xe/experiments/pokemondeck/model/vos/Deck.kt
@@ -4,7 +4,7 @@ package io.a2xe.experiments.pokemondeck.model.vos
  * Created by giorgio on 4/7/18.
  */
 data class Deck (val name: String,
-                 val cards: Array<Card> = emptyArray()) {
+                 val cards: List<Card> = listOf()) {
 
     val id: Int = Math.floor(Math.random() * 100000).toInt()
 
