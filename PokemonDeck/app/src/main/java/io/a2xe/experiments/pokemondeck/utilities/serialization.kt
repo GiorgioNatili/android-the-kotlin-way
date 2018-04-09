57b4ebc70d6ae4ca963e064ee97ff26f8c8b90a0
giorgionatili <g.natili@gnstudio.com>
2018-04-08 22:06:43 -0700

    Fix the JSON serialization

--- PokemonDeck/app/src/main/java/io/a2xe/experiments/pokemondeck/utilities/serialization.kt
+++ PokemonDeck/app/src/main/java/io/a2xe/experiments/pokemondeck/utilities/serialization.kt
@@ -1,8 +1,8 @@
 package io.a2xe.experiments.pokemondeck.utilities
 
 import com.google.gson.Gson
-
-
+import com.google.gson.reflect.TypeToken
+import io.a2xe.experiments.pokemondeck.model.vos.Deck
 
 /**
  * Created by giorgio on 4/8/18.
@@ -13,3 +13,12 @@ inline fun <reified T:Any> String.fromJsonString(dataType: Class<T>) : List<T> =
         Gson().fromJson<T>(this, dataType::class.java)
 )
 
+fun String.fromJsonToListOfDecks(): List<Deck> {
+
+    val collectionType = object : TypeToken<List<Deck>>(){}.type
+    return Gson().fromJson<List<Deck>>(this, collectionType)
+}
+
+fun <T:Any> String.fromJsonToListOfAnything(collectionType: TypeToken<List<T>>): List<T> {
+    return Gson().fromJson<List<T>>(this, collectionType.type)
+}
