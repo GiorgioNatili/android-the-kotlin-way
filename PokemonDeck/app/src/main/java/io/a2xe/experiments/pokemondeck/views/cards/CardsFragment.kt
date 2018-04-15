1e1a762874a9a49718c166eef3c68761ae8a8b86
giorgionatili <g.natili@gnstudio.com>
2018-04-14 17:00:24 -0700

    Fix the layout of the cards fragment

--- PokemonDeck/app/src/main/java/io/a2xe/experiments/pokemondeck/views/cards/CardsFragment.kt
+++ PokemonDeck/app/src/main/java/io/a2xe/experiments/pokemondeck/views/cards/CardsFragment.kt
@@ -2,26 +2,47 @@ package io.a2xe.experiments.pokemondeck.views.cards
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.a2xe.experiments.pokemondeck.R
+import io.a2xe.experiments.pokemondeck.model.repositories.CardsRepository
 import io.a2xe.experiments.pokemondeck.model.repositories.PokemonCardsRepository
 import io.a2xe.experiments.pokemondeck.services.PokemonCardsService
+import io.a2xe.experiments.pokemondeck.views.cards.adapter.CardsAdapter
+import io.a2xe.experiments.pokemondeck.views.cards.adapter.ItemOffsetDecoration
+import kotlinx.android.synthetic.main.fragment_cards.*
 
 class CardsFragment : Fragment() {
 
+    lateinit var cards: CardsRepository
+    lateinit var cardsAdapter: CardsAdapter
+    lateinit var gridLayoutManager: GridLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        println("onCreate CardsFragment")
 
         val service = PokemonCardsService.create()
+        cards = PokemonCardsRepository(service)
 
-        val pokemonCards = PokemonCardsRepository(service)
-        pokemonCards.searchCardsSet("sm5") {
-            println(it)
+        cards.searchCardsSet("sm5") {
+
+            cardsAdapter=  CardsAdapter(activity, it)
+            cards_list.adapter = cardsAdapter
         }
         // repo.getCardsDetails("sm5-4")
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+
+        super.onActivityCreated(savedInstanceState)
+
+        gridLayoutManager = GridLayoutManager(activity, 3)
+        cards_list.layoutManager = gridLayoutManager
+
+        cards_list.addItemDecoration(ItemOffsetDecoration(10))
     }
 
     override fun onCreateView(inflater: LayoutInflater?,
