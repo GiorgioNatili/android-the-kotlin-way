33484c78dcd0142cda5b1ff70af3e365fdbfa985
giorgionatili <g.natili@gnstudio.com>
2018-08-05 07:42:30 -0700

    Fix the hardcoded text

--- SimplifyAndroidDevelopment/app/src/main/java/io/a2xe/experiments/simplifyandroiddevelopment/MainActivity.kt
+++ SimplifyAndroidDevelopment/app/src/main/java/io/a2xe/experiments/simplifyandroiddevelopment/MainActivity.kt
@@ -2,13 +2,16 @@ package io.a2xe.experiments.simplifyandroiddevelopment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val defaultMessage = "Hello World!"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        hello_message.text = defaultMessage
     }
 }
