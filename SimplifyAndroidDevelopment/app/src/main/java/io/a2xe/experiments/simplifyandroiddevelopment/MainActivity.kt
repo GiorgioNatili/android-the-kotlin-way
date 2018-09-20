cbd16fd6c7169daf32d242422c7ca77e245057af
giorgionatili <g.natili@gnstudio.com>
2018-09-20 08:32:25 -0700

    Fix the example for chapter 1

--- SimplifyAndroidDevelopment/app/src/main/java/io/a2xe/experiments/simplifyandroiddevelopment/MainActivity.kt
+++ SimplifyAndroidDevelopment/app/src/main/java/io/a2xe/experiments/simplifyandroiddevelopment/MainActivity.kt
@@ -1,17 +1,36 @@
 package io.a2xe.experiments.simplifyandroiddevelopment
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val defaultMessage = "Hello World!"
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        hello_message.text = defaultMessage
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val name = sharedPreferences.getString("name", "")
+        hello_message.text = when(name.isNotEmpty()) {
+            true -> "Hello $name!"
+            else -> "Hello World!"
+        }
+
+        save.setOnClickListener {
+            sharedPreferences.edit {
+                putString("name", name_input.text.toString())
+            }
+
+            getString(R.string.name_updated).toast(this)
+        }
     }
 }
