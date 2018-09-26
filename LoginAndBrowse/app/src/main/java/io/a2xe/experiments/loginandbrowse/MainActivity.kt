9a72b11a3b46de55074639aaaf33691500dbea62
giorgionatili <g.natili@gnstudio.com>
2018-09-25 22:46:26 -0700

    Integrate utilities in main activity

--- LoginAndBrowse/app/src/main/java/io/a2xe/experiments/loginandbrowse/MainActivity.kt
+++ LoginAndBrowse/app/src/main/java/io/a2xe/experiments/loginandbrowse/MainActivity.kt
@@ -4,22 +4,24 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import io.a2xe.experiments.loginandbrowse.utilities.toast
 import kotlinx.android.synthetic.main.activity_main.*
-import toast
+import visibleOrInvisible
+
+const val RC_SIGN_IN = 123
 
 class MainActivity : AppCompatActivity() {
 
-    private val RC_SIGN_IN = 123
-
-    lateinit var authentication: FirebaseAuth
-    var authenticationProviders = arrayListOf(
+    val authenticationProviders = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.TwitterBuilder().build())
+
+    val currentUser: FirebaseUser?
+        get() = FirebaseAuth.getInstance().currentUser
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -30,10 +32,9 @@ class MainActivity : AppCompatActivity() {
             if (resultCode == Activity.RESULT_OK) {
 
                 // Successfully signed in
-                val user = FirebaseAuth.getInstance().currentUser
-                user?.let {
+                currentUser?.let {
                     "User ${it.uid} successfully logged in".toast(this)
-                    prepareForLogout(it)
+                    prepareAuthentication(it)
                 }
 
             } else {
@@ -50,41 +51,37 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        authentication = FirebaseAuth.getInstance()
-        val currentUser = authentication.currentUser
-
         when(currentUser) {
             null -> launchAuthenticationProviders(authenticationProviders)
-            else -> prepareForLogout(currentUser)
+            else -> prepareAuthentication(currentUser)
         }
 
-        twitter_logout.setOnClickListener { logoutFromTwitter(::prepareForAuthentication) }
+        twitter_logout.setOnClickListener { logoutFromTwitter { prepareAuthentication(currentUser) } }
         launch_providers.setOnClickListener { launchAuthenticationProviders(authenticationProviders) }
     }
 
-    fun prepareForLogout(currentUser: FirebaseUser) {
-
-        welcome_message.text = getString(R.string.hello_user, currentUser.uid)
-
-        twitter_logout.visibility = View.VISIBLE
-        launch_providers.visibility = View.GONE
-    }
-
-    fun prepareForAuthentication() {
-
-        welcome_message.text = ""
-
-        twitter_logout.visibility = View.GONE
-        launch_providers.visibility = View.VISIBLE
-    }
-
-    fun logoutFromTwitter(callback: ()->Unit) {
+    fun logoutFromTwitter(callback: () -> Unit) {
 
         AuthUI.getInstance()
                 .signOut(this)
                 .addOnCompleteListener {
                     callback.invoke()
                 }
+    }
+
+    fun prepareAuthentication(currentUser: FirebaseUser?) {
+
+        welcome_message.text = ""
+
+        currentUser?.let {
+            welcome_message.text = getString(R.string.hello_user, it.uid)
+        }
+
+        currentUser.takeIf { it != null }.also {
+
+            twitter_logout.visibleOrInvisible(it != null)
+            launch_providers.visibleOrInvisible(it == null)
+        }
     }
 
     fun launchAuthenticationProviders(providers: ArrayList<AuthUI.IdpConfig>) {
